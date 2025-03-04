2484f97037397a62cde62c4793582548376255e6
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-04 22:47:06 +0100

    Fix: Deployment issues

--- server.js
+++ server.js
@@ -19,64 +19,6 @@ console.log(os.type(),os.hostname(),os.version())
 
 
 
-// async function sendEmail() {
-//     try {
-//         const transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth: {
-//                 user: "suleiman20015kaita@gmail.com", // Set in .env file
-//                 pass:'rsmq mufs ycsa fhad' // Use App Password
-//             }
-//         });
-
-//         const info = await transporter.sendMail({
-//             from: "suleiman20015kaita@gmail.com",
-//             to: "suleiman76kaita@gmail.com",
-//             subject: "Ks Bank",
-//             html: `
-// //              <!DOCTYPE html>
-//         <html>
-//         <head>
-//             <meta charset="UTF-8">
-//             <meta name="viewport" content="width=device-width, initial-scale=1">
-//             <title>Email</title>
-//             <style>
-//                 body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }
-//                 .container { width: 100%; max-width: 600px; margin: 20px auto; background-color: #ffffff; border-radius: 10px;
-//                     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); overflow: hidden; }
-//                 .header { background-color: #007BFF; color: white; text-align: center; padding: 20px; font-size: 24px; font-weight: bold; }
-//                 .content { padding: 20px; color: #333; text-align: center; }
-//                 .content h1 { font-size: 28px; margin-bottom: 10px; color: #007BFF; }
-//                 .content p { font-size: 16px; line-height: 1.6; margin-bottom: 20px; }
-//                 .footer { background-color: #007BFF; color: white; text-align: center; padding: 10px; font-size: 14px; }
-//             </style>
-//         </head>
-//         <body>
-//             <div class="container">
-//                 <div class="header">
-//                     KS Bank
-//                 </div>
-//                 <div class="content">
-//                     <h1>Suleiman</h1>
-//                     <p>Nine Manu Kaita</p>
-//                 </div>
-//                 <div class="footer">
-//                     © 2024 KS Bank. All rights reserved.
-//                 </div>
-//             </div>
-//         </body>
-//         </html>
-//             `
-//         });
-
-//         console.log('Email sent successfully:', info.messageId);
-//     } catch (error) {
-//         console.error('Error sending email:', error);
-//     }
-// }
-
-// sendEmail();
-
 
 app.use(cors(opt));
 app.use(express.json());
@@ -123,7 +65,7 @@ app.use('/refresh', require('./routes/refresh'));
 mongoose.connection.once('open', () => {
     console.log("Connected to DB");
 
-    const server = app.listen(Port,"0.0.0.0", () => console.log("Listening on " + Port));
+    const server = app.listen(Port, () => console.log("Listening on " + Port));
 
     const io = new Server(server, {
         cors: { origin: ["http://localhost:5173","http://172.20.10.3:5173"], methods: ["GET", "POST"] }
