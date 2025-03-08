e27c73cd87769deb639384d54b7a96c36cdc9a4d
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-08 14:45:42 +0100

    change

--- controllers/logout.js
+++ controllers/logout.js
@@ -6,8 +6,9 @@ const logout=asynchandler(async(req,res)=>{
     const cookie=req.cookies;
     if(!cookie?.jwt)return res.status(401).json({"message":"Unauthorized"})
     res.clearCookie('jwt',{httpOnly:true,
-// sameSite:'none'
-sameSite: "Strict",secure:true})
+sameSite:'none'
+// sameSite: "Strict"
+,secure:true})
     res.status(201).json({"message":"you logout"})
     
 })
