556ec85c78f1396b7446c85c0330a13dfb0f7cf2
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-09 23:13:28 +0100

    ds

--- routes/refresh.js
+++ routes/refresh.js
@@ -1,16 +1,9 @@
-const verify = require("../middleware/verify")
-const refresh = require("../controllers/refresh")
-const express = require("express");
-const Router = express()
-
-// Middleware to set CORS headers
-Router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://ks-banks-7cpz.onrender.com/");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const verify=require("../middleware/verify")
+const refresh=require("../controllers/refresh")
+const express=require("express");
+const Router=express()
 
 Router.route('/')
 .get(refresh)
 // .post(logout)
-module.exports = Router;
+module.exports=Router;
