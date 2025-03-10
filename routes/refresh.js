0cd6c6a195de240e641d659615edadf7461b3c71
suleiman yusuf kaita <suleiman20015kaita@gmail.com>
2025-03-10 11:15:25 +0100

    kio

--- routes/refresh.js
+++ routes/refresh.js
@@ -1,9 +1,10 @@
-const verify=require("../middleware/verify")
-const refresh=require("../controllers/refresh")
-const express=require("express");
-const Router=express()
+const verify = require("../middleware/verify");
+const refresh = require("../controllers/refresh");
+const express = require("express");
+const Router = express.Router(); // Correct initialization of Router
 
 Router.route('/')
 .get(refresh)
 // .post(logout)
-module.exports=Router;
+
+module.exports = Router;
